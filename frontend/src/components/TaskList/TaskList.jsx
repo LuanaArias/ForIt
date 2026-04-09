58a645ca209392dd852ad1690f51cc8d4b527098
Luana <luanaariaaas@gmail.com>
2026-04-09 12:57:17 -0300

    agregando filtro de tareas

--- frontend/src/components/TaskList/TaskList.jsx
+++ frontend/src/components/TaskList/TaskList.jsx
@@ -13,6 +13,18 @@ import { TaskForm } from "../TaskForm/TaskForm.jsx";
 export function TaskList(){
     const [tasks, setTasks] = useState([]);
     const [editingTask, setEditingTask] = useState(null);
+    const [filter, setFilter] = useState("all");
+
+    const filteredTasks = tasks.filter(task => {
+        switch (filter) {
+            case "completed":
+                return task.completed == 1;
+            case "pending":
+                return task.completed == 0;
+            default:
+                return true;
+        }
+    });
 
     const cargarTareas = async () => {
         const data = await getTasks();
@@ -53,6 +65,29 @@ export function TaskList(){
 };
     return(
         <section>
+            <div className="d-flex justify-content-center gap-2 mb-4">
+                <button 
+                    className={`btn ${filter === "all" ? "btn-primary" : "btn-outline-primary"}`}
+                    onClick={() => setFilter("all")}
+                >
+                    Todas
+                </button>
+
+                <button 
+                    className={`btn ${filter === "pending" ? "btn-warning" : "btn-outline-warning"}`}
+                    onClick={() => setFilter("pending")}
+                >
+                    Pendientes
+                </button>
+
+                <button 
+                    className={`btn ${filter === "completed" ? "btn-success" : "btn-outline-success"}`}
+                    onClick={() => setFilter("completed")}
+                >
+                    Completadas
+                </button>
+            </div>
+
             <div className="container py-5">
                 <h1 className="h5 fw-bold mb-4 text-secondary text-center">Mis tareas</h1>
                 <div className="row justify-content-center">
@@ -60,7 +95,7 @@ export function TaskList(){
                     
                         <TaskForm onSubmit={handleSubmit} editingTask={editingTask}/>
                         <div className="mt-5">
-                            {tasks.map( t => ( 
+                            {filteredTasks.map( t => ( 
                             <TaskItem key={t.id} task={t} onDelete={handleDelete} onEdit={handleEdit} onToggle={handleToggle}/> ))}
                         </div>
                     </div>

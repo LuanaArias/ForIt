a56b3bb467c9260497096debc47a39f8a20af051
Luana <luanaariaaas@gmail.com>
2026-05-03 13:05:24 -0300

    actualizando index

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const PORT = process.env.PORT || 3000;
 const tasksRoutes = require("./routes/tasksRoutes")
 
 const app = express();

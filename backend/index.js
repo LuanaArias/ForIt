99d75c187d53ec146d11aa91a31ecc21497d9901
Luana <luanaariaaas@gmail.com>
2026-05-03 12:57:55 -0300

    configurando index.js para render

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ const tasksRoutes = require("./routes/tasksRoutes")
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://taskify-20ac7.web.app' // URL de Firebase
+}));
 app.use(express.json());
 app.use("/api", tasksRoutes);
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
     console.log(`Servidor corriendo en http://localhost:${process.env.PORT}`)
 })

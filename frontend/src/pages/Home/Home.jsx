fa090f31cf2d19146c6aee7348ce9ae827e39605
Luana <luanaariaaas@gmail.com>
2026-05-03 22:18:23 -0300

    arreglando login

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import { auth } from "../../firebase.js"; 
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { TaskList } from "../../components/TaskList/TaskList";
-import { Logo } from "../../components/ui/Logo";
 import './Home.css'
 import { LoadingScreen } from "../../components/LoadingScreen/LoadingScreen.jsx";
 import { WelcomeText } from "../../components/ui/WelcomeText/WelcomeText.jsx";
 import Login from "../../components/Auth/Login/Login.jsx";
+import { Header } from "../../components/Header/Header.jsx";
 
 export function Home(){
   const [user, setUser] = useState(null);
@@ -25,16 +25,7 @@ export function Home(){
   return (
     <main className={user ? "home-container" : "login-page"}>
 
-      <header className="home-header">
-        <Logo />
-        <p className="app-subtitle">Organizá tu día en segundos</p>
-
-        {user && (
-          <button onClick={() => signOut(auth)} className="logout-btn">
-            Cerrar Sesión
-          </button>
-        )}
-      </header>
+      <Header user={user} />
 
       {!user ? (
         <Login />
